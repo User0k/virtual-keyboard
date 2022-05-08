b06db6760e5973bb22ec4834950e4532eac23053
User0k <glucus2@yandex.ru>
2022-05-08 11:22:51 +0300

    feat: add highlighting pressed key

--- src/index.js
+++ src/index.js
@@ -6,3 +6,14 @@ import Keyboard from './Keyboard';
 renderDisplayH1();
 const keyboard = new Keyboard(localStorage.getItem('lang') ?? EN);
 keyboard.init();
+document.addEventListener('keydown', (e) => {
+  e.preventDefault();
+  const key = document.querySelector(`button[data-key=${e.code}]`);
+  key.classList.add('active');
+  keyboard.pressedKeys.add(e.code);
+});
+document.addEventListener('keyup', (e) => {
+  const key = document.querySelector(`button[data-key=${e.code}]`);
+  key.classList.remove('active');
+  keyboard.pressedKeys.delete(e.code);
+});
